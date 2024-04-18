d55c8dd875039f34b82a3263561e41e3c939f20c
AlexCPT-cpu <alexodey79@gmail.com>
2024-04-18 12:54:44 +0100

    create wallet

--- screens/CreateScene.js
+++ screens/CreateScene.js
@@ -1,3 +1,4 @@
+import path from "path";
 import { ethers } from "ethers";
 
 const CreateScene = (scene, text, replyObj, data) => {
@@ -6,6 +7,38 @@ const CreateScene = (scene, text, replyObj, data) => {
   });
   scene.on("text", (ctx) => {
     ctx?.deleteMessage();
+    const users = data.users || {};
+    const name = ctx.message.text;
+    if (!users.hasOwnProperty(ctx.chat.id)) {
+      // const provider = new ethers.JsonRpcProvider(process.env.INFURA_KEY)
+      const wallet = ethers.Wallet.createRandom();
+      const privateKey = wallet.privateKey;
+      users[ctx.chat.id] = {
+        accounts: [
+          {
+            private_key: privateKey,
+            name,
+          },
+        ],
+        pending_snipes: [],
+        id: ctx.chat.id,
+      };
+      const filePath = path.join(__dirname, "./constants/data.json");
+      const balance = 0.22;
+      const address = wallet.address;
+      try {
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+        const text = `\n✅ Default\n\n
+          ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n\n
+          💰 ${name} · ${balance} ETH\n
+          ${address}\n`;
+        const hyperlink = `<a href="https://etherscan.io/address/${address}">🌍 View on Etherscan</a>`;
+        const message = `${text}\n${hyperlink}`;
+        ctx.replyWithHTML(message);
+      } catch (error) {
+        console.error("Error writing data to file:", error);
+      }
+    }
   });
 };
 
